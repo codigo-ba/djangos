c74dd80b2ef4595522e1ca82befb06e9a4b1f3bd
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2025-04-05 19:06:36 +0000

    Improving signup view and correspondent html

--- users_app/user_stuff/views.py
+++ users_app/user_stuff/views.py
@@ -2,37 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html', {
-        'forms': UserCreationForm
-    })
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('reservation')
+        else:
+            return render(request, 'signup.html', {'forms': form})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('reservation')
-            except:
-                return render(request, 'signup.html', {
-                'forms': UserCreationForm,
-                'error' : "User already exists"
-            })
-              
-        return render(request, 'signup.html', {
-                'forms': UserCreationForm,
-                'error' : "Passwords do not match"
-        })
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'forms': form})
+    
 
 def reservation(request):
     return render(request, 'reservation.html')
+
 
 def signin(request):
     if request.method == 'GET':
